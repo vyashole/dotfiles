5bc63655de28e3a25c3b0978f8531c6d2451dab9
vyashole <adwait@socialcops.com>
2019-04-03 17:31:37 +0530

    fix vscode install for ubuntu 18.04wq

--- vscode_install.sh
+++ vscode_install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -eo pipefail
-sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
+sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt -y install code
 cat ~/.vscode_extensions | xargs -I {} code --install-extension {}
